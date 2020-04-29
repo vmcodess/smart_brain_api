4975eb1e5126441d593097dc48fd0ff96bbf39e9
vmatveev <vmatveevspam@gmail.com>
2020-04-29 03:48:45 -0400

    fix

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: process.env.DB_CLIENT,
     connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DATABASE
     }
 });
 
