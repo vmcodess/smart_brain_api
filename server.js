b672a75134adb7c1a3e54c67df0d140b027bd120
vmatveev <vmatveevspam@gmail.com>
2020-04-29 04:44:17 -0400

    fix

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-solid-94520',
       user : 'postgres',
       password : 'vlad',
       database : 'smart_brain'
