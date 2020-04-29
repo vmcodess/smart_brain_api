9aa60c5d20f944c2a9b4c53f50b3d09d7270aabf
vmatveev <vmatveevspam@gmail.com>
2020-04-29 03:53:23 -0400

    fix

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const image = require('./controllers/image');
 
 // connecting to database
 const db = knex({
-    client: process.env.DB_CLIENT,
+    client: 'pg',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB_DATABASE
+      host : 'postgresql-curved-34798',
+      user : 'postgres',
+      password : 'vlad',
+      database : 'smart_brain'
     }
 });
 
